c04524aae41013b33367121d05b912e539237da0
ayindesadiq01 <ayindesadiq01@gmail.com>
2023-02-09 17:10:23 +0100

    j

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+// let typed = new typed('.typing', {
+//  strings: ['Front-end Dev','Graphic Designer', 'HTML, CSS & JS DEV', 'Web Developer'],
+//  typeSpeed: 10,
+//  backSpeed: 100,
+//  loop: true,
+// })
+
+let typed = new Typed('.typing', {
+  strings: ["Front-End Dev", "Web Designer", "Graphic Designer",  "HTML, CSS, JS"],
+  typeSpeed: 50,
+  loop: true,
+});
